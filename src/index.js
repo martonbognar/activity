8e0717c7bad983c79f55a73173be8528fb35926c
marton bognar <martonbognar@sch.bme.hu>
2018-11-03 17:31:39 +0100

    Instructions, rounds, basic countdown

--- src/index.js
+++ src/index.js
@@ -2,21 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Word {
+  constructor(name, guessed) {
+    this.name = name;
+    this.guessed = guessed;
+  }
+}
+
 class Label extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: props.name,
-      guessed: false,
+      word: props.word,
     }
   }
 
   render() {
-    return (
-      <li>
-        {this.state.name}
-      </li>
-    )
+    if (!this.state.word.guessed) {
+      return (
+        <li>
+          {this.state.word.name}
+        </li>
+      )
+    } else {
+      return (
+        <li>
+          <del>{this.state.word.name}</del>
+        </li>
+      )
+    }
   }
 }
 
@@ -52,35 +66,116 @@ class NameForm extends React.Component {
   }
 }
 
-class Board extends React.Component {
+class Countdown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      labels: ['hello', 'world'],
-      setup: true,
+      seconds: props.seconds,
+      callback: props.callback,
     }
-    this.addLabel = this.addLabel.bind(this);
-    this.startGame = this.startGame.bind(this);
+
+    this.decreaseTime = this.decreaseTime.bind(this);
   }
 
-  addLabel(name) {
-    if (this.state.labels.indexOf(name) == -1) {
-      const labels = this.state.labels.slice();
-      labels.push(name);
-      this.setState({ labels: labels });
+  decreaseTime() {
+    if (this.state.seconds !== 0) {
+      let seconds = this.state.seconds - 1;
+      this.setState({ seconds: seconds });
+      setTimeout(this.decreaseTime, 1000);
+    } else {
+      this.state.callback();
     }
-  }
-
-  startGame() {
-    this.setState({setup: false});
   }
 
   render() {
     return (
       <div>
-        {this.state.setup && <div><NameForm callback={this.addLabel} /><input type="button" value="Start" onClick={this.startGame} /></div>}
+        {this.state.seconds} seconds remainig.
+        <input type="submit" value="Start" onClick={this.decreaseTime} />
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component {
+  rounds = ["Első kör nincs passz, akárhány tipp, mindent lehet, kivéve kimondani a keresett szót",
+    "Második körtől végig van passz, de csak egy tipp van, ha félre, akkor megy az aljára. Második körben max egy szó, mutogatás, hangutánzás bármennyi és mutogatás",
+    "Harmadik kör csak mutogatás",
+    "Negyedik kör egy póz"];
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      labels: [],
+      guessedLabels: [],
+      round: 0,
+    }
+    this.addLabel = this.addLabel.bind(this);
+    this.checkProgression = this.checkProgression.bind(this);
+    this.progressRound = this.progressRound.bind(this);
+    this.guessTop = this.guessTop.bind(this);
+    this.passTop = this.passTop.bind(this);
+  }
+
+  addLabel(name) {
+    if (name !== "" && this.state.labels.indexOf(name) === -1) { // TODO duplicate check
+      const labels = this.state.labels.slice();
+      labels.push(new Word(name, false));
+      this.setState({ labels: labels });
+    }
+  }
+
+  checkProgression() {
+    console.log(this.state.labels.length);
+    if (this.state.labels.length === 0) {
+      this.progressRound();
+      const labels = this.state.labels.slice();
+      const guessedLabels = this.state.guessedLabels.slice();
+      this.setState({ labels: guessedLabels, guessedLabels: labels });
+    }
+  }
+
+  progressRound() {
+    let round = this.state.round + 1;
+    this.setState({ round: round });
+  }
+
+  guessTop() {
+    const labels = this.state.labels.slice();
+    const guessedLabels = this.state.guessedLabels.slice();
+    const first = labels.shift();
+    first.guessed = true;
+    guessedLabels.push(first);
+    this.setState({ labels: labels, guessedLabels: guessedLabels }, this.checkProgression);
+  }
+
+  passTop() {
+    const labels = this.state.labels.slice();
+    const first = labels.shift();
+    labels.push(first);
+    this.setState({ labels: labels });
+  }
+
+  render() {
+    let controls;
+    if (this.state.round === 0) {
+      controls = <div><NameForm callback={this.addLabel} /><input type="button" value="Start" onClick={this.progressRound} /></div>;
+    } else {
+      controls = <div><input type="button" value="Guessed" onClick={this.guessTop} /><input type="button" value="Pass" onClick={this.passTop} /></div>
+    }
+
+    return (
+      <div>
+        {controls}
+        <div className="instructions">
+          {this.state.round > 0 && this.rounds[this.state.round - 1]}
+        </div>
+        <Countdown seconds="30" callback={this.passTop} />
         <ul>
-          {this.state.labels.map((element, index) => <Label key={element} name={element} />)}
+          {this.state.labels.map((element, index) => <Label key={element.name} word={element} />)}
+        </ul>
+        <ul>
+          {this.state.guessedLabels.map((element, index) => <Label key={element.name} word={element} />)}
         </ul>
       </div>
     );
