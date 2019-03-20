d27c35f75321accae2c0f5808efba58d6b79b7e1
marton bognar <martonbognar@sch.bme.hu>
2019-03-20 01:41:07 +0100

    Updates

---
+++ precache-manifest.484bca23bbc950f34faab356e3c54a6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1297ad06b16c87ea3976",
+    "url": "/static/css/main.f74b489f.chunk.css"
+  },
+  {
+    "revision": "1297ad06b16c87ea3976",
+    "url": "/static/js/main.1297ad06.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa14d41c3a7d47c2153004cff4faf0ed",
+    "url": "/index.html"
+  }
+];
